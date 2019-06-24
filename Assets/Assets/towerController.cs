2a10b865c74d1bf59688fd2b9ff9670d152bd61f
kwong1999 <wongkh@usc.edu>
2019-06-24 11:19:41 -0700

    tower =3d

--- Assets/Assets/towerController.cs
+++ Assets/Assets/towerController.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class towerController : MonoBehaviour, IPointerClickHandler
+public class towerController : MonoBehaviour
 {
     public RawImage img;
-    public RawImage t1;
-    public RawImage t2;
-    public RawImage t3;
-    public RawImage t4;
-    public void OnPointerClick(PointerEventData eventData)
-    {
+     GameObject t1;
+     GameObject t2;
+     GameObject t3;
+     GameObject t4;
         // OnClick code goes here ...
-        Debug.Log("hello");
-        img.enabled = true;
-        t1.enabled = false;
-        t2.enabled = false;
-        t3.enabled = false;
-        t4.enabled = false;
+        
 
 
 
-    }
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello");
+       t1 = GameObject.FindGameObjectWithTag("cell1");
+        t2 = GameObject.FindGameObjectWithTag("cell2");
+        t3 = GameObject.FindGameObjectWithTag("cell3");
+        t4 = GameObject.FindGameObjectWithTag("cell4");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("hello");
+        img.enabled = true;
+        t1.GetComponent<Renderer>().enabled = false;
+        t2.GetComponent<Renderer>().enabled = false;
+        t3.GetComponent<Renderer>().enabled = false;
+        t4.GetComponent<Renderer>().enabled = false;
     }
 }
