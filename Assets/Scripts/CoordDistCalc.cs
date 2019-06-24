90cb40185c1015161825f343f2cf4cb7f5a89538
kwong1999 <wongkh@usc.edu>
2019-06-24 11:58:07 -0700

    FB

--- Assets/Scripts/CoordDistCalc.cs
+++ Assets/Scripts/CoordDistCalc.cs
@@ -6,7 +6,7 @@ public class CoordDistCalc : MonoBehaviour
     public const double EarthRadiusInMiles = 3956.0;
     public const double EarthRadiusInKilometers = 6367.0;
     public const double EarthRadiusInMetres = 6371000;
-
+    
     public static double ToRadian(double val) { return val * (Math.PI / 180); }
     public static double DiffRadian(double val1, double val2) { return ToRadian(val2) - ToRadian(val1); }
 
@@ -19,8 +19,8 @@ public class CoordDistCalc : MonoBehaviour
     {
         double radius = CoordDistCalc.EarthRadiusInMiles;
 
-        if (m == GeoCodeCalcMeasurement.Kilometers) { radius = GeoCodeCalc.EarthRadiusInKilometers; }
-        if (m == GeoCodeCalcMeasurement.Metre) { radius = GeoCodeCalc.EarthRadiusInMetres; }
+        if (m == GeoCodeCalcMeasurement.Kilometers) { radius = CoordDistCalc.EarthRadiusInKilometers; }
+        if (m == GeoCodeCalcMeasurement.Metre) { radius = CoordDistCalc.EarthRadiusInMetres; }
         return radius * 2 * Math.Asin(Math.Min(1, Math.Sqrt((Math.Pow(Math.Sin((DiffRadian(lat1, lat2)) / 2.0), 2.0) + Math.Cos(ToRadian(lat1)) * Math.Cos(ToRadian(lat2)) * Math.Pow(Math.Sin((DiffRadian(lng1, lng2)) / 2.0), 2.0)))));
     }
 }
