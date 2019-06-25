4383309183fc76a7aa686753dbec64cfd622d4ba
kwong1999 <wongkh@usc.edu>
2019-06-25 08:49:08 -0700

    new scripts

---
+++ Assets/Assets/SliderVal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderVal : MonoBehaviour
+{
+    public Slider slid;
+    public Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = slid.value.ToString();
+
+        
+    }
+}
